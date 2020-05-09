ebecb72c216b49e6baa37a15188e00877c65ba3d
Kotikstar <52509515+Kotikstar@users.noreply.github.com>
2020-05-09 13:03:49 +0300

    Add files via upload

---
+++ reffer-user/config.php
@@ -0,0 +1,25 @@
+<?php
+if (! defined ( 'DATALIFEENGINE' )) {
+	die ( "ERROR" );
+} //защита файла
+$mysql_host = 'localhost';//хост БД
+$mysql_user = 'root';//юзер БД
+$mysql_pass = 'pass';//пароль БД
+$mysql_db = 'db';//база с БД
+
+$mysql_playtime_host = 'localhost';
+$mysql_playtime_user = 'root';
+$mysql_playtime_pass = 'pass';
+$mysql_playtime_db = 'playtime';
+
+$connect_error = 'Не удалось подсоеденится к базе данных!';
+$connect_error_select = 'Ошибка выбора бд!';
+$connect_error_playtime = 'Ошибка подключения к бд со временем !';
+$connect_error_select_playtime = 'Ошибка выбора бд времени!';
+
+
+
+if (!mysql_connect($mysql_host, $mysql_user, $mysql_pass)||!mysql_select_db($mysql_db)) {
+	die($connect_error);
+}
+?>
